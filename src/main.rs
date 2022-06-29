6362e405d02b5c949836e9a1f0a1e12b1dbd4afd
Overflow922 <genius922@yandex.ru>
2022-06-29 19:59:46 +0300

    Read text from file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Wrong number of params.");
+        return
+    }
 
     let query = &args[1];
     let filename = &args[2];
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
 }
